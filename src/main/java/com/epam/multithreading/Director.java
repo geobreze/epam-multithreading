d0e7f7b924e7f6d5c4dff61e3e970d5dc0d0632d
geobreze <geobreze@gmail.com>
2019-03-21 19:56:22 +0300

    main method split

--- src/main/java/com/epam/multithreading/Director.java
+++ src/main/java/com/epam/multithreading/Director.java
@@ -2,14 +2,10 @@ package com.epam.multithreading;
 
 import com.epam.multithreading.entity.Passenger;
 import com.epam.multithreading.entity.Taxi;
-import com.epam.multithreading.exception.ParseException;
 import com.epam.multithreading.logic.UberPark;
+import com.epam.multithreading.parse.JsonListParser;
 import com.epam.multithreading.parse.Parser;
-import com.epam.multithreading.parse.PassengerParser;
-import com.epam.multithreading.parse.TaxiParser;
 import com.epam.multithreading.requestor.AsyncRequest;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -18,36 +14,45 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Director {
-    private static final Logger LOGGER = LogManager.getLogger(Director.class);
     private static final String TAXIS_FILE = "taxis.json";
     private static final String PASSENGERS_FILE = "passengers.json";
 
     public static void main(String[] args) {
-        ClassLoader directorClassLoader = Director.class.getClassLoader();
-        InputStream taxisStream = directorClassLoader.getResourceAsStream(TAXIS_FILE);
-        InputStream passengersStream = directorClassLoader.getResourceAsStream(PASSENGERS_FILE);
-        Parser<Taxi> taxiParser = new TaxiParser();
-        Parser<Passenger> passengerParser = new PassengerParser();
+        initializePark();
 
-        try {
-            List<Taxi> taxis = taxiParser.parse(taxisStream);
-            List<Passenger> passengers = passengerParser.parse(passengersStream);
-            UberPark park = UberPark.getInstance();
-            park.setTaxis(taxis);
-
-            List<AsyncRequest> requests = new ArrayList<>();
-            for (Passenger passenger : passengers) {
-                requests.add(new AsyncRequest(passenger, UberPark.getInstance()));
-            }
-
-            ExecutorService executorService = Executors.newCachedThreadPool();
-            for (AsyncRequest request : requests) {
-                executorService.submit(request);
-            }
-
-            executorService.shutdown();
-        } catch (ParseException e) {
-            LOGGER.error(e);
+        List<AsyncRequest> requests = generateRequests();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (AsyncRequest request : requests) {
+            executorService.submit(request);
         }
+
+        executorService.shutdown();
+    }
+
+    private static List<AsyncRequest> generateRequests() {
+        InputStream passengersStream = getResourceStream(PASSENGERS_FILE);
+        Parser<Passenger> passengerParser = new JsonListParser<>();
+        List<Passenger> passengers = passengerParser.parse(passengersStream, Passenger.class);
+
+        UberPark park = UberPark.getInstance();
+        List<AsyncRequest> requests = new ArrayList<>();
+        for (Passenger passenger : passengers) {
+            requests.add(new AsyncRequest(passenger, park));
+        }
+        return requests;
+    }
+
+    private static void initializePark() {
+        InputStream taxisStream = getResourceStream(TAXIS_FILE);
+        Parser<Taxi> taxiParser = new JsonListParser<>();
+        List<Taxi> taxis = taxiParser.parse(taxisStream, Taxi.class);
+
+        UberPark park = UberPark.getInstance();
+        park.setTaxis(taxis);
+    }
+
+    private static InputStream getResourceStream(String resourcePath) {
+        ClassLoader directorClassLoader = Director.class.getClassLoader();
+        return directorClassLoader.getResourceAsStream(resourcePath);
     }
 }
